2ee72e01f6a6ad6ff15d570015800187fa08d8d9
maribelcuales <maribelcuales@gmail.com>
2022-06-26 12:19:26 +0800

    Added hooks from thirdweb to App.js.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react'; 
+
 const App = () => {
+  // Using the hooks from thirdweb 
+  const address = useAddress(); 
+  const connectWithMetamask = useMetamask(); 
+  console.log("👋 Address:", address);
+
   return (
     <div className="landing">
       <h1>Welcome to My DAO</h1>
