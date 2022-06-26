95d246d3abe44cd954b074b1439007af3718aa94
maribelcuales <maribelcuales@gmail.com>
2022-06-26 12:54:09 +0800

    Added case if user has not connected wallet.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,19 @@ const App = () => {
   const connectWithMetamask = useMetamask(); 
   console.log("👋 Address:", address);
 
+  // case if user has not connected their wallet 
+  // let them call connectWallet 
+  if (!address) {
+    return (
+      <div className='landing'>
+        <h1>Welcome to MiddleEarthDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your Wallet
+        </button>
+      </div>
+    );
+  }
+  
   return (
     <div className="landing">
       <h1>Welcome to My DAO</h1>
