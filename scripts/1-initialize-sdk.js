79a24394238969eddf88fe8b945f8a827299d406
maribelcuales <maribelcuales@gmail.com>
2022-06-28 21:16:30 +0800

    Write function to initialize sdk.

--- scripts/1-initialize-sdk.js
+++ scripts/1-initialize-sdk.js
@@ -24,4 +24,15 @@ const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_API_UR
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 const sdk = new ThirdwebSDK(wallet);
 
+(async () => {
+  try {
+    const address = await sdk.getSigner().getAddress();
+    console.log("SDK initialized by address:", address)
+  } catch (err) {
+    console.error("Failed to get apps from the sdk", err);
+    process.exit(1);
+  }
+})();
 
+// export initialized thirdweb SDK for use in other scripts 
+export default sdk; 
