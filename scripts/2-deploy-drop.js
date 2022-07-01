95c0e7fafbae02daf96d18d0aed5d070e09e6950
maribelcuales <maribelcuales@gmail.com>
2022-07-01 23:07:47 +0800

    Set address of nft sale recipient to AddressZero so users will not be charged for the drop.

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -6,9 +6,16 @@ import { readFileSync } from 'fs';
   try {
     const editionDropAddress = await sdk.deployer.deployEditionDrop({
       // the collection's name
-      name: "MiddleEarth Guilds",
+      name: "MiddleEarth Guild",
       // description for the collection 
-      description: "A DAO for all creatures of Middle Earth. ",
+      description: "A DAO for all creatures of Middle Earth.",
+      // the image that will be held as the NFT 
+      image: readFileSync(),
+      // pass in here the address of the person who will be receiving the proceeds from sale of nfts 
+      // But we won' charge people for the drop, so pass in the 0x0 address 
+      // you can set this to own wallet address if we want to charge for the drop 
+      primary_sale_recipient: AddressZero; 
+
     }); 
   }
 } catch (error) {
