528c38109d3ff54ac3fc284b19f5a1dc6fc6c76e
maribelcuales <maribelcuales@gmail.com>
2022-07-04 20:58:34 +0800

    Added script to initialize the contract on thirdweb sdk.

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -6,20 +6,32 @@ import { readFileSync } from 'fs';
   try {
     const editionDropAddress = await sdk.deployer.deployEditionDrop({
       // the collection's name
-      name: "MiddleEarth Guild",
+      name: "MiddleEarthDAO Membership",
       // description for the collection 
-      description: "A DAO for all creatures of Middle Earth.",
+      description: "A DAO for all folks of Middle Earth.",
       // the image that will be held as the NFT 
       image: readFileSync(),
       // pass in here the address of the person who will be receiving the proceeds from sale of nfts 
       // But we won' charge people for the drop, so pass in the 0x0 address 
       // you can set this to own wallet address if we want to charge for the drop 
-      primary_sale_recipient: AddressZero; 
-
+      primary_sale_recipient: AddressZero,
     }); 
-  }
-} catch (error) {
+
+    // this initialization returns the address of our contract 
+    // use this to initialize the contract on the thirdweb sdk 
+    const editionDrop = sdk.getEditionDrop(editionDropAddress);
+
+    // get the metadata of contract 
+    const metadata = await editionDrop.metadata.get();
+
+    console.log(
+      "✅ Successfully deployed editionDrop contract, address:",
+      editionDropAddress,
+    );
+    console.log("✅ editionDrop metadata:", metadata); 
+  } catch (error) {
   console.log("Failed to deploy editionDrop contract", error);
+  }
 })();
 
 
