43c681dddb8b7b0c0a6c108c29f62c833e0b1ce0
maribelcuales <maribelcuales@gmail.com>
2022-07-28 17:50:30 +0800

    Fixed error in token contract address.

--- scripts/6-print-money.js
+++ scripts/6-print-money.js
@@ -1,7 +1,7 @@
 import sdk from './1-initialize-sdk.js';
 
 // Insert Token Contract Address of ERC-20 contract printed in previous file
-const token = sdk.getToken(0x7c2c645734e89b6f10D429528D04cFB8c3bD27C5); 
+const token = sdk.getToken("0x7c2c645734e89b6f10D429528D04cFB8c3bD27C5"); 
 
 (async () => {
   try {
