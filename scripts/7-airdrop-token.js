00e3a54b7e2f2f42fd363702ea750b014529c8e1
maribelcuales <maribelcuales@gmail.com>
2022-07-28 18:45:00 +0800

    Added comments to airdrop token.

--- scripts/7-airdrop-token.js
+++ scripts/7-airdrop-token.js
@@ -1,14 +1,14 @@
 import sdk from "./1-initialize-sdk.js";
 
-// This is the address to our ERC-1155 membership NFT contract.
+// Address to our ERC-1155 membership NFT contract.
 const editionDrop = sdk.getEditionDrop("0x72f0948daFC590f31f9c3F729EaaFA0147EB799e");
 
-// This is the address to our ERC-20 token contract.
+// Address to our ERC-20 token contract.
 const token = sdk.getToken("0x7c2c645734e89b6f10D429528D04cFB8c3bD27C5");
 
 (async () => {
   try {
-    // Grab all the addresses of people who own our membership NFT, 
+    // Grab all the addresses of people who own the membership NFT, 
     // which has a tokenId of 0.
     const walletAddresses = await editionDrop.history.getAllClaimerAddresses(0);
 
@@ -25,7 +25,7 @@ const token = sdk.getToken("0x7c2c645734e89b6f10D429528D04cFB8c3bD27C5");
       const randomAmount = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000);
       console.log("✅ Going to airdrop", randomAmount, "tokens to", address);
 
-      // Set up the target.
+      // Set up the target
       const airdropTarget = {
         toAddress: address,
         amount: randomAmount,
@@ -42,3 +42,4 @@ const token = sdk.getToken("0x7c2c645734e89b6f10D429528D04cFB8c3bD27C5");
     console.error("Failed to airdrop tokens", err);
   }
 })();
+
