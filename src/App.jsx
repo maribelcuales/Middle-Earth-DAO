f0c39712236e7586fd3025d8f88ca4691f7c2ab1
maribelcuales <maribelcuales@gmail.com>
2022-08-01 22:45:46 +0800

    Check if user has already voted or not.

--- src/App.jsx
+++ src/App.jsx
@@ -39,28 +39,56 @@ const App = () => {
 
   // Give our app access to our vote
   const [proposals, setProposals] = useState([]);
-  const [isVoting, setIsVoting] = useState(false); 
-  const [hasVoted, setHasVoted] = useState(false); 
+  const [isVoting, setIsVoting] = useState(false);
+  const [hasVoted, setHasVoted] = useState(false);
 
-  // Retrieve all our existing proposals from the contract 
+  // Retrieve all our existing proposals from the contract
   useEffect(() => {
     if (!hasClaimedNFT) {
-      return; 
+      return;
     }
 
-    // A simple call to vote.getAll() to grab the proposals 
+    // A simple call to vote.getAll() to grab the proposals
     const getAllProposals = async () => {
       try {
         const proposals = await vote.getAll();
-        setProposals(proposals); 
+        setProposals(proposals);
         console.log("🌈 Proposals: ", proposals);
       } catch (error) {
         console.log("Failed to get proposals", error);
       }
     };
-    getAllProposals(); 
+    getAllProposals();
   }, [hasClaimedNFT, vote]);
- 
+
+  // We also need to check if the user has already voted 
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // If we haven't finished retrieving the proposals from the useEffect above 
+    // then we can't check if the user voted yet 
+    if (!proposals.length) {
+      return;
+    } 
+
+    const checkIfUserHasVoted = async () => {
+      try {
+        const hasVoted = await vote.hasVoted(proposals[0].proposalId, address);
+        setHasVoted(hasVoted);
+        if (hasVoted) {
+          console.log("🥵 User has already voted!");
+        } else {
+          console.log("🙂 User has not voted yet."); 
+        }
+      } catch (error) {
+        console.error("Failed to check if wallet has voted", error);  
+      }
+    }; 
+    checkIfUserHasVoted();
+  }, [hasClaimedNFT, proposals, address, vote]); 
+
   // This useEffect grabs all the addresses of our members holding our NFT
   useEffect(() => {
     if (!hasClaimedNFT) {
